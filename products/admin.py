b864ba60711ae6ff10433b1ca0aac2742ca06f73
LucaPetrocchi <l.petrocchi@itecriocuarto.org.ar>
2024-04-11 21:06:52 -0300

    fieldsets + new fields

--- products/admin.py
+++ products/admin.py
@@ -1,20 +1,65 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 
 from products.models import Product, Category
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    ordering = ("name", "price")
+    search_fields = ("price", "category", "desc", "name")
+    list_filter = ("category", "name")
+    list_editable = ("category", "price", "stock", "desc")
     list_display = [
-        'name',
-        'category',
-        'price', 
-        'desc',
-        ]
-    
+        "name",
+        "category",
+        "price",
+        "stock",
+        "desc",
+        "get_price_range",
+        "get_total",
+        "get_stock",
+    ]
+
+    fieldsets = [
+        (
+            "product info",
+            {
+                "fields": [
+                    "name",
+                    "price",
+                    "desc",
+                ]
+            },
+        ),
+        (
+            "more info",
+            {
+                "classes": ['collapse'],
+                "fields": ['stock',],
+            }
+        )
+    ]
+
+    def get_total(self, obj):
+        return obj.price * obj.stock
+
+    def get_stock(self, obj):
+        poco = "#ff0000"
+        mucho = "#008000"
+        normal = "#ffd300"
+        if obj.stock < 100:
+            codigo = poco
+        elif obj.stock > 300:
+            codigo = mucho
+        else:
+            codigo = normal
+
+        return format_html('<span style="color: {}"> {} </span>', codigo, obj.stock)
+
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = [
-        'name'
-    ]
+    list_display = ["name"]
--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.db import models
 
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
@@ -8,17 +10,25 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    desc = models.TextField()
+    desc = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=777, null=True)
 
     category = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL,
-        null=True, blank=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True
     )
+
+    @admin.display(description="price range")
+    def get_price_range(self):
+        if self.price > 1000000:
+            return "PRECIAZO ALTARRRRRRRDO BROOOOOO"
+        if 500000 < self.price < 1000000:
+            return "its mid lol"
+        if self.price < 500000:
+            return "LOL"
 
     def __str__(self):
         return self.name
-    
