976b3a6f97865855101613772f905a94cac91339
LucaPetrocchi <l.petrocchi@itecriocuarto.org.ar>
2024-04-18 21:02:15 -0300

    repository

---
+++ products/repositories/product.py
@@ -0,0 +1,51 @@
+from products.models import (
+    Product, 
+    Category,
+)
+from typing import (
+    List,
+    Optional,
+)
+
+class ProductRepository:
+    def create(
+        self,
+        name: str,
+        price: float,
+        stock: int,
+        category: Optional[Category] = None,
+        desc: Optional[str] = "PELOTUDO",
+    ) -> Product.objects:
+        return Product.objects.create(
+            name=name,
+            price=price,
+            desc=desc,
+            stock=stock,
+            category=category,
+        )
+    
+    def get_all(self) -> List[Product]:
+        return Product.objects.all()
+    
+    def get_by_id(self, id) -> Optional[Product]:
+        return Product.objects.filter(id=id).first()
+
+    def get_by_price_range(
+        self,
+        min_price: float,
+        max_price: float,
+    ) -> List[Product]:
+        return Product.objects.filter(
+            price__range=(min_price, max_price)
+        )
+
+    def get_by_category(
+        self,
+        category_id: int,
+    ) -> List[Product]:
+        return Product.objects.filter(category=category_id).first()
+
+
+
+
+
